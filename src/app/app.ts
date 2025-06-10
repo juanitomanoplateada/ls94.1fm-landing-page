34e079c0c473bf35151fa20d5fd382e92a8b5243
EdwinNino-24 <edwinnino2404@gmail.com>
2025-06-10 11:33:54 -0500

    v1.0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Footer } from "./components/footer/footer";
+import { Header } from "./components/header/header";
+import { Hero } from "./components/hero/hero";
+import { AboutUs } from "./components/about-us/about-us";
+import { Contact } from "./components/contact/contact";
+import { Schedule } from "./components/schedule/schedule";
+import { Live } from "./components/live/live";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Footer, Header, Hero, AboutUs, Contact, Schedule, Live],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/live/live.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live',
+  imports: [],
+  templateUrl: './live.html',
+  styleUrl: './live.scss'
+})
+export class Live {
+
+}
